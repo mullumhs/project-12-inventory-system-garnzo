c651215d3b2c1f7b2ec40b1602bc848df214233c
Garnzo <92022699+garnzo@users.noreply.github.com>
2024-06-04 11:50:52 +1000

    got started on the structure of my lab 2 code
    it was a bit hard to understand at first but no stress now

--- lab2.py
+++ lab2.py
@@ -10,20 +10,39 @@
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 
 # Step 1: Import the Item class from lab1.py
-
-
-
-
+from lab1 import Item
 
 # Step 2: Define the InventoryManager class as a facade to handle the inventory operations.
 # It should include methods to add, remove, update, and display items in the inventory.
+class InventoryManager:
+    
+    def __init__(self, item_name, price, qty):
+        pass
 
+    def add_item(self, item_name, price, qty):
+        pass
 
+    def remove_item(self, item_name):
+        pass
 
+    def update_item(self, item_name, new_item):
+        pass
 
+    def update_price(self, item_name, price):
+        pass
+
+    def update_qty(self, item_name, qty):
+        pass
+
+    def display_items(self):
+        pass
+    
+    def find_item(self, item_name):
+        pass
 
 # Step 2: Create instances of the Item class and InventoryManager, then demonstrate their usage.
 # E.g. add items to the inventory, remove items, update items, and display the inventory.
 
-
+inv = InventoryManager()
+inv.add_item("Johnny Depp Cigars 3 Pack", "$150.00", 150)
 
