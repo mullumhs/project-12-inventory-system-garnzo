8580f76b92c0f4089f3ea3beb68a2e5078d6bc93
Garnzo <92022699+garnzo@users.noreply.github.com>
2024-06-11 14:58:04 +1000

    help

--- lab2.py
+++ lab2.py
@@ -20,39 +20,42 @@ class InventoryManager:
         self._items = []
 
     def add_item(self, item_name, price, qty):
-        for item_name in self._items:
-            if item_name == self._items:
+        for item in self._items:
+            if item_name == item.get_item():
                 raise ValueError("Item already in list")
         self._items.append(item_name)
 
     def remove_item(self, item_name):
-        for item_name in self._items:
-            if item_name != self._items:
-                raise ValueError("Item Doesn't Exist")
-        self._items.remove(item_name)
+        for item in self._items:
+            if item_name == item.get_item():
+                self._items.remove(item_name)          
+        raise ValueError("Item Doesn't Exist")
 
-    def update_item(self, item_name, new_item):
-        for item_name in self._items:
-            if new_item != self._items:
-                raise ValueError("Item Doesn't Exist")
-        self._items.remove(item_name)
-        self._items.append(new_item)
-
+    def update_item(self, item_name, new_item_name):
+        for item in self._items:
+            if item_name == item.get_item():
+                item.set_item(new_item_name)       
+        raise ValueError("Item Doesn't Exist")
+        
     def update_price(self, item_name, price, new_price):
-        for item_name in self._items:
-            if item_name != self._items:
-                raise ValueError("Item Doesn't Exist")
-        self._items.remove(item_name)
-        self._items.append(item_name, new_price)
-
-    def update_qty(self, item_name, qty):
-        pass
+        for item in self._items:
+            if item_name == item.get_item():
+                item.set_price(new_price)
+    def update_qty(self, item_name, new_qty):
+        for item in self._items:
+            if item_name == item.get_item():
+                item.set_qty(new_qty)
+        raise ValueError("Error")
 
     def display_items(self):
-        pass
+        for item in self._items:
+            return(self._items)
     
     def find_item(self, item_name):
-        pass
+        for item in self._items:
+            if item_name == item.get_item():
+                return(f"Item Found {item}")
+                
 
 # Step 2: Create instances of the Item class and InventoryManager, then demonstrate their usage.
 # E.g. add items to the inventory, remove items, update items, and display the inventory.
